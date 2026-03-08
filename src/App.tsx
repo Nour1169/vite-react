5887e267f83856fca2ba9ad17ab09032c55724bb
Nour1169 <Mnour2001@hotmail.com>
2026-03-08 12:28:51 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [stage, setStage] = useState<"hidden" | "quote" | "form">("hidden");
